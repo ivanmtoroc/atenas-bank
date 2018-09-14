61dac5c1cf7b4457bd80003ee6c139abc00c26c4
ivanmtoroc <ivanmtoroc@gmail.com>
2018-09-14 11:27:02 -0500

    feature: base added

--- atenasbank/urls.py
+++ atenasbank/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
+# View
+from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.test, name = 'test'),
 ]
