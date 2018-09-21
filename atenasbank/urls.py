ccbd1034258b35406e2de3ad0d1f5530feddea3a
ivanmtoroc <ivanmtoroc@gmail.com>
2018-09-21 15:11:29 -0500

    feature: hello world

--- atenasbank/urls.py
+++ atenasbank/urls.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-
-# View
-from . import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.test, name = 'test'),
+    path('users/', include('apps.users.urls', namespace='users')),
 ]
