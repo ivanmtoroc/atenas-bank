c57b2f3b20cb0afd299e2c2cfbe1ccb8ba87db7a
Iván Toro <ivanmtoroc@gmail.com>
2018-10-18 23:31:36 -0500

    feature: template base updated

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SignupForm
 
 
-def hello(request):
-    return render(request, 'users/hello.html')
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('users:signup')
+    else:
+        form = SignupForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'users/new-user.html', context)
