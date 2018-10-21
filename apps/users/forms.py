7f9201ad09e2dc86fec8d91448424622e6721692
Iván Toro <ivanmtoroc@gmail.com>
2018-10-21 18:01:28 -0500

    feature: modified users CRUD

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -1,12 +1,12 @@
-from django import forms
 from django.contrib.auth.models import User
-
+from django import forms
 from .models import Profile
 
 
 class LoginForm(forms.Form):
     username = forms.CharField(min_length = 4, max_length = 20)
     password = forms.CharField(max_length = 30, widget = forms.PasswordInput)
+
 
 class NewUserForm(forms.Form):
     username = forms.CharField(min_length = 4, max_length = 20)
--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import new_user, login_view, logout_view, change_status
+from .views import users, login_view, logout_view, delete
 
 
 app_name = 'users'
 
 urlpatterns = [
-    path('', new_user, name='new'),
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
-    path('change_status/', change_status, name='change_status'),
+    path('', users, name = 'crud'),
+    path('login/', login_view, name = 'login'),
+    path('logout/', logout_view, name = 'logout'),
+    path('delete/', delete, name = 'delete'),
 ]
