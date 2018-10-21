bf999b85817f6d2e355094eb890c479b703c8e54
Iván Toro <ivanmtoroc@gmail.com>
2018-10-21 00:27:51 -0500

    feature: status and info of users

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from . import views
+from .views import new_user, login_view, logout_view, change_status
 
 
 app_name = 'users'
 
 urlpatterns = [
-    path('', views.signup, name='signup'),
-    path('login/', views.login, name='login'),
+    path('', new_user, name='new'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('change_status/', change_status, name='change_status'),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,21 +1,65 @@
-from django.shortcuts import render, redirect
-from .forms import NewUserForm
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.http.response import JsonResponse
+from .forms import NewUserForm, LoginForm
 from .models import Profile
 
-
-def signup(request):
+@login_required
+def new_user(request):
     if request.method == 'POST':
         form = NewUserForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('users:login')
+            return redirect('users:new')
     else:
         form = NewUserForm()
     context = {
         'form': form,
         'profiles': Profile.list()
     }
-    return render(request, 'users/new-user.html', context)
+    return render(request, 'users/new_user.html', context)
 
-def login(request):
-    return render(request, 'users/login.html')
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('users:login')
+
+def login_view(request):
+    context = {
+        'form': LoginForm()
+    }
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username = username, password = password)
+        if user:
+            login(request, user)
+            return redirect('users:new')
+        else:
+            context['error'] = 'Invalid username or password'
+            return render(request, 'users/login.html', context)
+    return render(request, 'users/login.html', context)
+
+@login_required
+def change_status(request):
+    if request.is_ajax():
+        id = request.POST.get('id', None)
+        try:
+            user = User.objects.get(id = id)
+            user.is_active = not user.is_active
+            user.save()
+            if user.is_active:
+                messages.success(request, 'User successfully activated')
+            else:
+                messages.success(request, 'User successfully deactivated')
+        except User.DoesNotExist as e:
+            messages.error(request, 'User does not exist')
+    else:
+        messages.error(request, 'Unauthorized access')
+    data = {
+        'url': reverse('users:new')
+    }
+    return JsonResponse(data)
