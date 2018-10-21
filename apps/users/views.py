ae30b6b127c5968e2083ff68550a6408354831d9
Iván Toro <ivanmtoroc@gmail.com>
2018-10-21 18:01:28 -0500

    feat: modified users CRUD

--- apps/users/views.py
+++ apps/users/views.py
@@ -7,44 +7,26 @@ from django.http.response import JsonResponse
 from .forms import NewUserForm, LoginForm
 from .models import Profile
 
+
 @login_required
-def new_user(request):
-    if request.method == 'POST':
-        form = NewUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('users:new')
-    else:
-        form = NewUserForm()
+def users(request):
     context = {
-        'form': form,
         'profiles': Profile.list()
     }
-    return render(request, 'users/new_user.html', context)
-
-@login_required
-def logout_view(request):
-    logout(request)
-    return redirect('users:login')
-
-def login_view(request):
-    context = {
-        'form': LoginForm()
-    }
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username = username, password = password)
-        if user:
-            login(request, user)
-            return redirect('users:new')
-        else:
-            context['error'] = 'Invalid username or password'
-            return render(request, 'users/login.html', context)
-    return render(request, 'users/login.html', context)
+        form_create = NewUserForm(request.POST)
+        if form_create.is_valid():
+            form_create.save()
+            return redirect('users:crud')
+        context['new'] = True
+    else:
+        form_create = NewUserForm()
+        form_update = NewUserForm()
+    context['form_create'] = form_create
+    return render(request, 'users/users.html', context)
 
 @login_required
-def change_status(request):
+def delete(request):
     if request.is_ajax():
         id = request.POST.get('id', None)
         try:
@@ -60,6 +42,29 @@ def change_status(request):
     else:
         messages.error(request, 'Unauthorized access')
     data = {
-        'url': reverse('users:new')
+        'url': reverse('users:crud')
     }
     return JsonResponse(data)
+
+
+def login_view(request):
+    context = {
+        'form': LoginForm()
+    }
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username = username, password = password)
+        if user:
+            login(request, user)
+            return redirect('users:crud')
+        else:
+            context['error'] = 'Invalid username or password'
+            return render(request, 'users/login.html', context)
+    return render(request, 'users/login.html', context)
+
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('users:login')
