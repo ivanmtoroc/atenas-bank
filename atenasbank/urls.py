e85905db86d6f86e5593abd3fbf617faa2397505
Iván Toro <ivanmtoroc@gmail.com>
2018-10-23 18:57:24 -0500

    feat: tickets app

--- atenasbank/urls.py
+++ atenasbank/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('users/', include('apps.users.urls', namespace='users')),
     path('home/', include('apps.home.urls', namespace='home')),
+    path('tickets/', include('apps.tickets.urls', namespace='tickets')),
 ]
