824e5b76ca07761165cbee24f5c55fed5ab38e81
ivanmtoroc <ivanmtoroc@gmail.com>
2018-10-24 15:56:55 -0500

    feature: template base of tickets app

--- apps/tickets/models.py
+++ apps/tickets/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class TicketRequestForm(models.Model):
+    ACTIVITIES = (
+        ('CRP', 'Consignaciones, retiros y pagos de servicios'),
+        ('IE', 'Importaciones y exportaciones'),
+        ('S', 'Seguros'),
+        ('TD', 'Transacciones en dolares'),
+        ('VIP', 'Clientes VIP')
+    )
+    activity = models.CharField(max_length = 20, choices = ACTIVITIES)
+    cedula = models.CharField(max_length = 10)
