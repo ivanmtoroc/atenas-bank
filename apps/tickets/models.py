7c4d79e0f3fa34fc7bb8b2842a42a68a80350c92
Iván Toro <ivanmtoroc@gmail.com>
2018-10-31 00:05:53 -0500

    feat: users CRUD

--- apps/tickets/models.py
+++ apps/tickets/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
+
+ACTIVITIES = (
+    ('CRP', 'Consignaciones, retiros y pagos de servicios'),
+    ('IE', 'Importaciones y exportaciones'),
+    ('S', 'Seguros'),
+    ('TD', 'Transacciones en dolares'),
+    ('VIP', 'Clientes VIP')
+)
+
 class TicketRequestForm(models.Model):
-    ACTIVITIES = (
-        ('CRP', 'Consignaciones, retiros y pagos de servicios'),
-        ('IE', 'Importaciones y exportaciones'),
-        ('S', 'Seguros'),
-        ('TD', 'Transacciones en dolares'),
-        ('VIP', 'Clientes VIP')
-    )
     activity = models.CharField(max_length = 20, choices = ACTIVITIES)
     cedula = models.CharField(max_length = 10)
--- apps/users/views.py
+++ apps/users/views.py
@@ -5,18 +5,15 @@ from .forms import LoginForm
 
 
 def login_view(request):
-    context = {
-        'form': LoginForm()
-    }
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username = username, password = password)
         if user:
             login(request, user)
-            return redirect('users:crud')
+            return redirect('users:users')
+    context = { 'form': LoginForm() }
     return render(request, 'users/login.html', context)
-
 
 @login_required
 def logout_view(request):
