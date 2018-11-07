bb980baa6bcbfcc558c326fb8f5eb4fdb922ae2e
Iván Toro <ivanmtoroc@gmail.com>
2018-11-06 23:31:11 -0500

    feature: ticket request

--- apps/users/models.py
+++ apps/users/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 
 POSITIONS = (
-    ('Operator', 'Operator'),
-    ('Manager', 'Manager')
+    ('OPT', 'Operator'),
+    ('MNG', 'Manager')
 )
 
 class User(AbstractUser):
