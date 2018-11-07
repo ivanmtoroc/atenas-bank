aaa5ebb6cb85c033a208f25582dd8dd266c871fd
Iván Toro <ivanmtoroc@gmail.com>
2018-11-06 23:31:11 -0500

    feat: ticket request

--- apps/tickets/models.py
+++ apps/tickets/models.py
@@ -9,6 +9,15 @@ ACTIVITIES = (
     ('VIP', 'Clientes VIP')
 )
 
-class TicketRequestForm(models.Model):
-    activity = models.CharField(max_length = 20, choices = ACTIVITIES)
+class Ticket(models.Model):
+    number = models.AutoField(primary_key = True)
     cedula = models.CharField(max_length = 10)
+    vip = models.BooleanField(default = False)
+    activity = models.CharField(max_length = 20, choices = ACTIVITIES, default = 'CRP')
+    state = models.BooleanField(default = False)
+    date = models.DateField(auto_now_add = True)
+    start = models.DateField(null = True)
+    end = models.DateField(null = True)
+
+    def __str__(self):
+        return '%s%s' % (self.activity, self.number)
