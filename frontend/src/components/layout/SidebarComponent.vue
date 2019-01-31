cfc1cdeebe735aab176f54c5bbea1d6061adbb81
Iván Toro <ivanmtoroc@gmail.com>
2019-01-31 00:14:54 -0500

    feat: project divided

---
+++ frontend/src/components/layout/SidebarComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <aside class="main-sidebar">
+    <section class="sidebar">
+      <ul class="sidebar-menu" data-widget="tree">
+        <li class="active">
+          <router-link to="/">
+            <i class="fa fa-bank"></i><span>Home</span>
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/users">
+            <i class="fa fa-users"></i><span>Users</span>
+          </router-link>
+        </li>
+      </ul>
+    </section>
+  </aside>
+</template>
