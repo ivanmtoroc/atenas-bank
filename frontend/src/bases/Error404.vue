60266790a456139f878e965ace23094f158053e7
Iván Toro <ivanmtoroc@gmail.com>
2019-02-04 23:14:08 -0500

    feat: router and store

---
+++ frontend/src/bases/Error404.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Erro 404: Server not found! :(</h1>
+</template>
--- frontend/src/components/layout/SidebarComponent.vue
+++ frontend/src/components/layout/SidebarComponent.vue
@@ -3,12 +3,12 @@
     <section class="sidebar">
       <ul class="sidebar-menu" data-widget="tree">
         <li class="active">
-          <router-link to="/">
-            <i class="fa fa-bank"></i><span>Home</span>
+          <router-link :to="{ name: 'dashboard' }">
+            <i class="fa fa-bank"></i><span>Dashboard</span>
           </router-link>
         </li>
         <li>
-          <router-link to="/users">
+          <router-link :to="{ name: 'users' }">
             <i class="fa fa-users"></i><span>Users</span>
           </router-link>
         </li>
