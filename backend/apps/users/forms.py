3fb06eea548df12fe3bd7f41f664d4f4e54735a4
Iván Toro <ivanmtoroc@gmail.com>
2019-02-05 17:47:27 -0500

    feature: users api initial configuration

--- backend/apps/users/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-
-class LoginForm(forms.Form):
-    username = forms.CharField(min_length = 4, max_length = 20)
-    password = forms.CharField(max_length = 30, widget = forms.PasswordInput)
--- backend/apps/users/viewsets.py
+++ backend/apps/users/viewsets.py
@@ -1,8 +1,10 @@
+# Django
+from apps.users.serializers import UserSerializer
+from apps.users.models import User
+
+# Django Rest Framework
 from rest_framework.response import Response
 from rest_framework import viewsets
-from .serializers import UserSerializer
-from .models import User
-
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
